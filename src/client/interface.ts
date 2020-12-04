b4701f24cb18179dbe0d9c576a99cf68ac1894be
mher96 <mher.mshecyan@gmail.com>
2020-12-04 23:56:00 +0400

    file spaces configuration

--- src/client/interface.ts
+++ src/client/interface.ts
@@ -2,12 +2,12 @@ import {User, Pong, Auth} from './types'
 import {UserApi} from '../user/interface'
 
 interface ClientInterface {
-        ping(): Promise<Pong>;
-        createUser(username: string, email: string, password: string): Promise<User>;
-        verify(code:string): Promise<User>;
-        login(username: string, password: string): Promise<Auth>;
-        setToken(token: string): void;
-        user(): UserApi
+    ping(): Promise<Pong>;
+    createUser(username: string, email: string, password: string): Promise<User>;
+    verify(code:string): Promise<User>;
+    login(username: string, password: string): Promise<Auth>;
+    setToken(token: string): void;
+    user(): UserApi
 }
 
 export default ClientInterface
